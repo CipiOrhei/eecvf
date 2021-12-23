217c9ac89af8e6f2ab5b2b7d44c2faa04d236e3c
Ciprian Orhei PC <ciprian.orhei@gmail.com>
2021-12-23 08:11:19 +0200

    Modified histogram job to output in separate port folder Fixed small bug in name of RGB histogram equalization job

--- main_sharpening_dilated.py
+++ main_sharpening_dilated.py
@@ -32,19 +32,7 @@ def main():
 
     for el in eval_list:
         Application.do_histogram_job(port_input_name=el)
-    #
-    # sharpen = Application.do_sharpen_filter_job(port_input_name='RAW', is_rgb=True, kernel=CONFIG.FILTERS_SECOND_ORDER.LAPLACE_1)
-    # sharpen_2 = Application.do_sharpen_filter_job(port_input_name='RAW', is_rgb=True, kernel=CONFIG.FILTERS_SECOND_ORDER.LAPLACE_2)
-    # sharpen_dil = Application.do_sharpen_filter_job(port_input_name='RAW', is_rgb=True, kernel=CONFIG.FILTERS_SECOND_ORDER.LAPLACE_DILATED_5x5_1)
-    # um_dil = Application.do_unsharp_filter_job(port_input_name='RAW', is_rgb=True)
-    # um_dil = Application.do_unsharp_filter_expanded_job(port_input_name='RAW', is_rgb=True, kernel=CONFIG.FILTERS_SECOND_ORDER.LAPLACE_1, strenght=1)
-    # um_dil = Application.do_unsharp_filter_expanded_job(port_input_name='RAW', is_rgb=True, kernel=CONFIG.FILTERS_SECOND_ORDER.LAPLACE_DILATED_5x5_1, strenght=1)
-    #
-    # Application.do_histogram_job(port_input_name=grey)
-    # Application.do_histogram_job(port_input_name=hist_eq)
-    # Application.do_histogram_job(port_input_name=sharpen)
-    # Application.do_histogram_job(port_input_name=sharpen_2)
-    # Application.do_histogram_job(port_input_name=sharpen_dil)
+
     Application.create_config_file()
     Application.configure_save_pictures(ports_to_save='ALL', job_name_in_port=True)
     # Application.configure_show_pictures(ports_to_show='ALL', time_to_show=500, to_rotate=False)
